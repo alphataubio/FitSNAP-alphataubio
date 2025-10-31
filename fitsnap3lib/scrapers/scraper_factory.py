7fc826dbbf60d542600e3d446ed1a810d358aec4
alphataubio <alphataubio@gmail.com>
2025-10-30 21:00:22 -0400

    remove fairchem_scraper

--- fitsnap3lib/scrapers/scraper_factory.py
+++ fitsnap3lib/scrapers/scraper_factory.py
@@ -4,27 +4,18 @@ from fitsnap3lib.scrapers.json_scraper import Json
 from fitsnap3lib.scrapers.xyz_scraper import XYZ
 from fitsnap3lib.scrapers.vasp_scraper import Vasp
 
-# only import LMDB scraper if required modules are available
-import importlib.util
-if (importlib.util.find_spec("lmdb") is not None and 
-    importlib.util.find_spec("ase") is not None):
-    from fitsnap3lib.scrapers.fairchem_scraper import FAIRChem
-else:
-    FAIRChem = None
-
 # only import ADIOS2 scraper if adios2 is available
+import importlib.util
 if importlib.util.find_spec("adios2") is not None:
     from fitsnap3lib.scrapers.adios2_scraper import ADIOS2
 else:
     ADIOS2 = None
-
 
 def scraper(scraper_name, pt, config):
     """Section Factory"""
     instance = search(scraper_name)
     instance.__init__(scraper_name, pt, config)
     return instance
-
 
 def search(scraper_name):
     instance = None
