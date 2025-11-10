de12dbedffd492ea036511cd11dc584e51dec418
alphataubio <alphataubio@gmail.com>
2025-11-09 23:26:31 -0500

    Update AlNi_PYACE_SLATE_ARD.sh

--- examples/AlNi_PYACE_SLATE_ARD/AlNi_PYACE_SLATE_ARD.sh
+++ examples/AlNi_PYACE_SLATE_ARD/AlNi_PYACE_SLATE_ARD.sh
@@ -8,6 +8,8 @@
 
 module load StdEnv/2023 intel/2024.2.0 openblas/0.3.27 openmpi/5.0.3 python/3.11.5 hdf5-mpi/1.14.5 mpi4py/4.0.0 scipy-stack/2025a
 
+source ~/fitsnap-venv/bin/activate
+
 export PYTHONPATH=~/scratch/FitSNAP-alphataubio:~/scratch/lammps-alphataubio/python:$PYTHONPATH
 export LD_LIBRARY_PATH=~/.local/lib64:~/scratch/lammps-alphataubio/build-fitsnap:$LD_LIBRARY_PATH
 
