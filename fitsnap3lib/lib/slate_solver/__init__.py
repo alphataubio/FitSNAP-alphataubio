3cd6fc0dc535d11072b6381107094458d4460747
alphataubio <alphataubio@gmail.com>
2026-03-30 14:45:02 -0400

    cleanup

--- fitsnap3lib/lib/slate_solver/__init__.py
+++
@@ -1,7 +0,0 @@
-"""SLATE ridge solver module"""
-try:
-    from .slate_wrapper import ridge_solve_qr
-    __all__ = ['ridge_solve_qr']
-except ImportError:
-    ridge_solve_qr = None
-    __all__ = []
