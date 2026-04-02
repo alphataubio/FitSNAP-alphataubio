fdff8947c45fa13f0e47091e92bb4715e2d90bae
alphataubio <alphataubio@gmail.com>
2026-04-02 09:29:33 -0400

    Update slate.py

--- fitsnap3lib/solvers/slate.py
+++ fitsnap3lib/solvers/slate.py
@@ -271,7 +271,7 @@ class SLATE(SlateValidation):
             coef_change_str = " " if coef_old_ is None else f" coef_rel_change {coef_rel_change:.2g} coef_abs_change {coef_abs_change:.2g}"
             coef_old_ = np.copy(coef_)
 
-            pt.single_print(f"SLATE ARD #{iteration} ({elapsed_iteration/60:.1f}m): alpha {alpha_:.3g} sse {sse_:.3g} cond_number {cond_number:.1g} alpha {alpha_:.4f} gamma_sum {gamma_active.sum():.1f} n_active {n_active}{coef_change_str}{slurm_time_left_str}")
+            pt.single_print(f"SLATE ARD #{iteration} ({elapsed_iteration/60:.1f}m): alpha {alpha_:.4g} sse {sse_:.3g} cond_number {cond_number:.1g} gamma_sum {gamma_active.sum():.1f} n_active {n_active}{coef_change_str}{slurm_time_left_str}")
             
             iteration += 1
             
